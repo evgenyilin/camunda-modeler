dda5bfeb62fb32969cfbeb6e7d214c7090ee9ca7
Philipp Fromme <philippfromme@outlook.com>
2020-08-09 10:56:53 +0200

    feat(client): add element templates modal

---
+++ client/src/plugins/element-templates-modal/components/__tests__/InputSpec.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.
+ *
+ * Camunda licenses this file to you under the MIT; you may not use this file
+ * except in compliance with the MIT License.
+ */
+
+import React from 'react';
+
+import { mount } from 'enzyme';
+
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import Input from '../Input';
+
+chai.should();
+chai.use(sinonChai);
+
+
+
+describe('<Input>', function() {
+
+
+  it('should render', function() {
+
+    // when
+    const wrapper = mount(<Input />);
+
+    // then
+    expect(wrapper).to.exist;
+  });
+
+
+  it('#onChange', function() {
+
+    // given
+    const onChangeSpy = sinon.spy();
+
+    const wrapper = mount(<Input onChange={ onChangeSpy } />);
+
+    // when
+    wrapper.find('input').first().simulate('change', { target: { value: 'foo' } });
+
+    // then
+    expect(onChangeSpy).to.have.been.calledWith('foo');
+  });
+
+
+  it('#onClear', function() {
+
+    // given
+    const onChangeSpy = sinon.spy();
+
+    const wrapper = mount(<Input value="foo" onChange={ onChangeSpy } />);
+
+    // when
+    wrapper.find('button').first().simulate('click');
+
+    // then
+    expect(onChangeSpy).to.have.been.calledWith('');
+  });
+
+});
